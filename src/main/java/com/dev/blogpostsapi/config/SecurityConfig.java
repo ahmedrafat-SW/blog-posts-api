b43ec2fabbae341fc5c6804f69f2343eb8489ab1
Ahmed Rafat <129176607+ahmedrafat-SW@users.noreply.github.com>
2023-08-11 17:53:54 +0300

    updating pom file with spring security dependency

--- src/main/java/com/dev/blogpostsapi/config/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.dev.blogpostsapi.config;
-
-public class SecurityConfig {
-
-}
--- src/main/java/com/dev/blogpostsapi/controller/UserController.java
+++ src/main/java/com/dev/blogpostsapi/controller/UserController.java
@@ -5,9 +5,8 @@ import com.dev.blogpostsapi.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Optional;
 
